c72984943b7b8d0d57442736db109f9130fbd8e4
Błażej Święcicki <blazej.swiecicki@pixers.pl>
2016-11-09 10:48:44 +0100

    kostka set-ip can now configure the interface to get it's address from dhcp

--- kostka/commands/set_ip.py
+++ kostka/commands/set_ip.py
@@ -7,21 +7,18 @@ from .enter import enter
 
 @cli.command(name='set-ip')
 @click.option('-i', '--interface', default='host0')
+@click.option('--gateway', help='Default gateway for the interface')
 @click.argument('name')
 @click.argument('cidr')
 @click.pass_context
 @require_existing_container
-def set_ip(ctx, name, cidr, interface):
-    """ Sets an ip address for the container's veth interface """
-    ip, netmask = cidr.split('/', 1)
+def set_ip(ctx, name, cidr, interface, gateway):
+    """ Sets an ip address for a container's network interface.
 
-    # We want it to work both for running and not running containers.
-    # If the container is running, we have to set it's ip without rebooting it.
-    if is_active(name):
-        ctx.invoke(enter, name=name, cmd=('ip', 'link', 'set', interface, 'up'))
-        ctx.invoke(enter, name=name, cmd=('ip', 'address', 'flush', 'dev', interface))
-        ctx.invoke(enter, name=name, cmd=('ip', 'address', 'add', cidr, 'dev', interface))
-
+        Instead of passing a static IP address, you can use
+        `kostka set-ip CONTAINER dhcp` to get the address from
+        a DHCP server.
+    """
     path = '/var/lib/machines/' + name
     if is_active('var-lib-machines-{}-fs.mount'.format(name)):
         # Now if the overlayfs is mounted, we should use it:
@@ -39,13 +36,27 @@ def set_ip(ctx, name, cidr, interface):
             pass
 
         with open('etc/network/interfaces.d/' + interface, 'w') as f:
-            template = """
-            auto {interface}
-            iface {interface} inet static
-                address {ip}
-                netmask {netmask}
-            """.format(ip=ip, netmask=netmask, interface=interface)
-            template = re.sub(r"^ {8}", "", template, flags=re.MULTILINE).lstrip()
+            if cidr == 'dhcp':
+                template = """
+                auto {interface}
+                iface {interface} inet dhcp
+                """.format(interface=interface)
+            else:
+                ip, netmask = cidr.split('/', 1)
+                template = """
+                auto {interface}
+                iface {interface} inet static
+                    address {ip}
+                    netmask {netmask}
+                """.format(ip=ip, netmask=netmask, interface=interface)
+                if gateway is not None:
+                    template += "    gateway {gateway}\n".format(gateway=gateway)
+            template = re.sub(r"^ {16}", "", template, flags=re.MULTILINE).lstrip()
             f.write(template)
     finally:
         os.chdir(cwd)
+
+    # We want it to work both for running and not running containers.
+    # If the container is running, we have to set it's ip without rebooting it.
+    if is_active(name):
+        ctx.invoke(enter, name=name, cmd=('ifup', interface))
