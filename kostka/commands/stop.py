6875c71d8e15c836b7a9e9a8f6757e3646925eb9
Błażej Święcicki <blazej.swiecicki@pixers.pl>
2016-11-09 12:04:03 +0100

    kostka stop is now extensible

--- kostka/commands/stop.py
+++ kostka/commands/stop.py
@@ -1,12 +1,15 @@
 import click
 import subprocess
-from ..utils import cli, require_existing_container, run_hooks
+from ..utils import cli, require_existing_container, run_hooks, Container
+from ..plugins import extensible_command
 
 
 @cli.command()
+@extensible_command
 @click.argument("name")
 @require_existing_container
-def stop(name):
+def stop(name, extensions):
     run_hooks('pre-stop', name)
     subprocess.check_call(['/bin/systemctl', 'stop', name])
+    extensions(Container(name))
     run_hooks('post-stop', name)
